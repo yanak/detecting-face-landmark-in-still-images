904ee0cf1f66368a254ea8932868930e00359afd
Yasunori Tanaka (田中 穏識) <tanaka_yasunori@cyberagent.co.jp>
2020-03-30 22:52:23 +0900

    Initial Commit

---
+++ DetectingFaceLandmarkInStillImages/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  DetectingFaceLandmarkInStillImages
+//
+//  Created by 田中 穏識 on 2020/03/30.
+//  Copyright © 2020 田中 穏識. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
